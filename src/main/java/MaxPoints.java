7c6eb06ea390294160c78a443fb0115fdb751e21
GarryXian <475613393@qq.com>
2020-03-27 09:02:25 +0800

    经过最多点的直线

--- src/main/java/MaxPoints.java
+++ src/main/java/MaxPoints.java
@@ -19,13 +19,55 @@ public class MaxPoints {
 //        int[][] points = {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
 //        int[][] points = {{1,1},{2,2},{1,1},{2,2}};
 //        int[][] points = {{0,0},{0,0}};
-        int[][] points = {{0,0},{94911151,94911150},{94911152,94911151}};
+//        int[][] points = {{0,0},{94911151,94911150},{94911152,94911151}};
+        int[][] points = {{1,1},{2,2},{3,1},{-1,3},{-10,1}};
 //        int[][] points = {{0, 0}, {0, 0}, {0, 0}};
 //        int[][] points = {{0, -1}, {0, 3}, {0, -4}, {0, -2}, {0, -4}, {0, 0}, {0, 0}, {0, 1}, {0, -2}, {0, 4}};
-        System.out.println(maxPoints(points));
+        System.out.println(maxPointsOp(points));
     }
 
-    // 更换解题方法, 暴力解题, 保存线上的所有点,
+    public int maxPointsOp(int[][] points) {
+        if (points.length < 3) {
+            return points.length;
+        }
+        int res = 0;
+        //遍历每个点
+        for (int i = 0; i < points.length; i++) {
+            int duplicate = 0;
+            int max = 0;//保存经过当前点的直线中，最多的点
+            HashMap<String, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < points.length; j++) {
+                //求出分子分母
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+                if (x == 0 && y == 0) {
+                    duplicate++;
+                    continue;
+
+                }
+                //进行约分
+                int gcd = gcd(x, y);
+                x = x / gcd;
+                y = y / gcd;
+                String key = x + "@" + y;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                max = Math.max(max, map.get(key));
+            }
+            //1 代表当前考虑的点，duplicate 代表和当前的点重复的点
+            res = Math.max(res, max + duplicate + 1);
+        }
+        return res;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
     // 定义直线的数据结构: 端点(数组下标), 斜率, 线上坐标的数量, 使用Map存储, key 为 端点下标 + 斜率 拼接字符串
     public static int maxPoints(int[][] points) {
         Map<Integer, Integer> eachMaxPoints = new HashMap<>();
